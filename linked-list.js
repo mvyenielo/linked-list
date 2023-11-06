cafdc59a129bf6c13e60a120c3c62cf75922bc3e
ross-brown <rbrown_98@yahoo.com>
2023-11-06 14:33:56 -0600

    finish adding all methods to LL

--- linked-list.js
+++ linked-list.js
@@ -89,7 +89,7 @@ class LinkedList {
   /** shift(): return & remove first item. */
 
   shift() {
-    if(!this.length) {
+    if (!this.length) {
       throw new Error("list is empty");
     }
 
@@ -113,31 +113,119 @@ class LinkedList {
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+    if (idx > this.length - 1 || idx < 0) throw new Error("Invalid index");
 
+    let index = 0;
+    let current = this.head;
+
+    while (current !== null) {
+      if (index === idx) return current.val;
+
+      current = current.next;
+      index++;
+    }
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
+    if (idx > this.length - 1 || idx < 0) throw new Error("Invalid index");
 
+    let index = 0;
+    let current = this.head;
+
+    while (current !== null) {
+      if (index === idx) {
+        current.val = val;
+        return;
+      }
+
+      current = current.next;
+      index++;
+    }
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    if (idx > this.length || idx < 0) throw new Error("Invalid index");
 
+    let index = 0;
+    let current = this.head;
+    let newNode = new Node(val);
+
+    if (idx === 0) {
+      this.unshift(val);
+      return;
+    }
+
+    if (idx === this.length) {
+      this.push(val);
+      return;
+    }
+
+    while (current !== null) {
+      if (index === idx - 1) {
+        let newNext = current.next;
+        current.next = newNode;
+        newNode.next = newNext;
+        this.length++;
+        return;
+      }
+      current = current.next;
+    }
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
+    if (idx > this.length - 1 || idx < 0) throw new Error("Invalid index");
 
+    if (this.length === 1) {
+      let onlyNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+
+      return onlyNode.val;
+    }
+
+    if (idx === 0) {
+      let removedNode = this.shift();
+      return removedNode;
+    }
+
+    let index = 0;
+    let current = this.head;
+
+    while (current !== null) {
+      if (index === idx - 1) {
+        console.log("if ran");
+        let removedNode = current.next;
+        current.next = current.next.next;
+        this.length--;
+        return removedNode.val;
+      }
+      index++;
+      current = current.next;
+    }
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
+    if (!this.length) return 0;
 
+    let sum = 0;
+
+    let current = this.head;
+
+    while (current !== null) {
+      sum += current.val;
+      current = current.next;
+    }
+
+    return sum / this.length;
   }
 }
 
