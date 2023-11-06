2921870c589c794010b952c233f0e2beabcb003c
Megan <mvyenielo58@gmail.com>
2023-11-06 11:51:50 -0800

    added methods for push, unshift, pop, and shift for linked lists

--- linked-list.js
+++ linked-list.js
@@ -23,25 +23,91 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
+    let newNode = new Node(val);
 
+    if (this.head === null) {
+      this.head = newNode;
+    }
+
+    if (this.tail !== null) {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+
+    this.length++;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
+    let newNode = new Node(val);
 
+    if (this.head !== null) {
+      newNode.next = this.head;
+    }
+
+    if (this.tail === null) {
+      this.tail = newNode;
+    }
+
+    this.head = newNode;
+
+    this.length++;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    if (!this.length) {
+      throw new Error("list is empty");
+    }
 
+    if (this.length === 1) {
+      let onlyNode = this.head;
+      this.head = null;
+      this.tail = null;
+
+      this.length--;
+      return onlyNode.val;
+    }
+
+    let current = this.head;
+    while (current !== null) {
+      if (current.next === this.tail) {
+        current.next = null;
+        let tail = this.tail;
+        this.tail = current;
+
+        this.length--;
+        return tail.val;
+      }
+      current = current.next;
+    }
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    if(!this.length) {
+      throw new Error("list is empty");
+    }
 
+    if (this.length === 1) {
+      let onlyNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+
+      return onlyNode.val;
+    }
+
+    let poppedNode = this.head;
+    this.head = this.head.next;
+
+    this.length--;
+
+    return poppedNode.val;
   }
 
   /** getAt(idx): get val at idx. */
@@ -71,7 +137,7 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
-    
+
   }
 }
 
